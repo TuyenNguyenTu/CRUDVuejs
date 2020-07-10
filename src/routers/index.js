163d3b1f7a196190f2f076c3693804edda941dca
Quyen-nv <Quyen-nv@best-inc.com>
2020-07-10 11:41:13 +0700

    first commit

---
+++ src/routers/index.js
@@ -0,0 +1,28 @@
+import Router from 'vue-router';
+import Vue from 'vue';
+
+import Home from '@/components/Home';
+import Contact from '@/components/Contact';
+import Login from '@/components/Login';
+Vue.use(Router);
+
+let router = new Router({
+    mode: 'history',
+    routes: [{
+            path: '/',
+            name: 'Login',
+            component: Login
+        },
+        {
+            path: '/list-data',
+            name: 'Home',
+            component: Home
+        },
+        {
+            path: '/contact',
+            name: 'Contact',
+            component: Contact
+        }
+    ]
+})
+export default router;
